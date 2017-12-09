014cf6851de74a38317678b563cd363a25d6dbb6
Vorh <sorcerer12@list.ru>
2017-12-09 23:49:51 +0300

    [ ADD ] build script

--- src/compile.sh
+++ src/compile.sh
@@ -1,8 +1,27 @@
 #!/usr/bin/env bash
 
 
-nasm -f elf -F stabs ./hello_world.asm
 
-ld -m elf_i386 hello_world.o -o ./helloworld
+function compile(){
+    nasm -f elf -F stabs $1 -o result.o >&2
+    ld -m elf_i386 result.o -o ./result
+    ./result
 
-./helloworld
+}
+
+
+while getopts ":f:d" opt; do
+    case $opt in
+    f)
+        compile $OPTARG
+    ;;
+    d)
+        rm result
+        rm result.o
+    ;;
+    \?)
+    echo "Specify fail" -$OPTARG >&2
+    ;;
+    esac
+done
+
