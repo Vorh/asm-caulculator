ea131a0a8573c39c609d24c392ef831a0c6a6b5b
Vorh <sorcerer12@list.ru>
2017-12-10 00:24:32 +0300

    [ ADD ] repeat lessons

--- src/hello_repeat.asm
+++ src/hello_repeat.asm
@@ -1,17 +1,33 @@
 SECTION .data
-msg     db      'Hello World!', 0Ah
+msg     db      'Hello, brave new world!', 0Ah
  
 SECTION .text
 global  _start
  
 _start:
  
-    mov     edx, 13
-    mov     ecx, msg
-    mov     ebx, 1
-    mov     eax, 4
-    int     80h
- 
-    mov     ebx, 0
-    mov     eax, 1
-    int     80h
+    mov ebx, msg
+    mov eax, ebx
+
+nextchar:
+
+    cmp byte [eax], 0
+    jz  finished
+    inc eax
+    jmp nextchar
+
+
+
+finished:
+
+    sub eax, ebx
+
+    mov edx, eax
+    mov ecx, msg
+    mov ebx, 1
+    mov eax, 4
+    int 80H
+
+    mov ebx, 0
+    mov eax, 1
+    int 80h
