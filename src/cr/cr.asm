94cf8b0d7db5446f6af610f45de9bb1f1608eb93
Vorh <sorcerer12@list.ru>
2017-12-31 14:16:22 +0300

    [ ADD ] calculate

--- src/cr/cr.asm
+++ src/cr/cr.asm
@@ -1,7 +1,27 @@
-%include 'print.asm'
 
+extern print
 
 SECTION .text
 global _start
 
-       _start:
+     _start:
+         pop     ecx
+
+     nextArg:
+         cmp     ecx, 0h
+         jz      noMoreArgs
+         pop     eax
+         push    eax
+         call    print
+         dec     ecx
+         jmp     nextArg
+
+     noMoreArgs:
+         call    quit
+
+    quit:
+
+        mov ebx, 0
+        mov eax, 1
+        int 80H
+        ret
--- src/cr/string.asm
+++ src/cr/string.asm
@@ -1,4 +1,47 @@
+%include 'syscall.asm'
 
-print:
+
+global print
+global strlen
+
+SECTION .text
+
+
+    ; Write string
+    print:
+        push ebp
+        mov  ebp,esp
+        push dword [ebp+8]
+        call strlen
+        add esp, 4
+        syscall 4,1, [ebp+8],eax
+        mov esp,ebp
+        pop ebp
+        ret
+
+
+    ; Calculates the length
+    ; return: eax
+    strlen:
+        push ebp
+        push eax
+        push esi
+        mov ebp,esp
+        xor eax, eax
+        mov esi, [ebp+8] ; First argument
+    .lp:
+        cmp byte [esi],0
+        jz  .quit
+        inc esi
+        inc eax
+        jmp short .lp
+    .quit:
+        pop ebp
+        pop eax
+        pop esi
+        ret
+
+
+
 
 
