1581d2d5e1a4c96c2bf8d3cc40add7e968d7f63c
Vorh <sorcerer12@list.ru>
2018-01-02 22:15:20 +0300

    not work

--- src/cr/cr.asm
+++ src/cr/cr.asm
@@ -7,11 +7,23 @@ extern quit
 SECTION .data
 
 nameMsg db 'HI bitch',0
+arguments resb 20
 
 SECTION .text
 global _start
 
      _start:
+         pop ecx
+     nextArg:
+         cmp     ecx, 0h
+         jz      noMoreArgs
+         pop     eax
+         mov     arguments, eax
+         dec     ecx
+         jmp     nextArg
+
+     noMoreArgs:
+
          push dword nameMsg
          call printLN
 
