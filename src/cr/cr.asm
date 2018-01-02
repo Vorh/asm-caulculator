2aea8b71ea9c98072db2afea050a2b79d447d333
Vorh <sorcerer12@list.ru>
2018-01-02 20:47:25 +0300

    [ ADD ] printLN

--- src/cr/cr.asm
+++ src/cr/cr.asm
@@ -1,17 +1,29 @@
 
 extern print
+extern printLN
 extern quit
 extern sprint
+
+
+SECTION .data
+
+nameMsg db 'HI',0
 
 SECTION .text
 global _start
 
      _start:
-         pop     ecx
+
+         push dword nameMsg
+         call printLN
+
+         pop ecx
 
      nextArg:
          cmp     ecx, 0h
          jz      noMoreArgs
+         pop     eax
+         push    eax
          call    print
          sub     esp, 4
          dec     ecx
--- src/cr/string.asm
+++ src/cr/string.asm
@@ -2,9 +2,14 @@
 
 
 global print
+global printLN
 global strlen
 global sprint
 
+
+SECTION .data
+
+new_line db 0AH,0
 
 SECTION .text
 
@@ -19,6 +24,21 @@ SECTION .text
         syscall 4,1, [ebp+8],eax
         mov esp,ebp
         pop ebp
+        ret
+
+
+    printLN:
+        push ebp
+        mov  ebp,esp
+
+        push dword [ebp+8] ; first argument
+        call print
+
+        push new_line
+        call print
+
+        mov  esp,ebp
+        pop  ebp
         ret
 
 
@@ -42,45 +62,7 @@ SECTION .text
 
 
 
-    slen:
 
-        push ebx
-        mov  ebx,eax
-
-    .nextchar:
-
-        cmp  byte [eax], 0
-        jz   finished
-        inc  eax
-        jmp  nextchar
-
-    .finished:
-
-        sub eax,ebx
-        pop ebx
-        ret
-
-
-    sprint:
-
-        push edx
-        push ecx
-        push ebx
-        push eax
-        call slen
-
-        mov edx, eax
-        pop eax
-
-        mov ecx, eax
-        mov ebx, 1
-        mov eax, 4
-        int 80H
-
-        pop ebx
-        pop ecx
-        pop edx
-        ret
 
 
 
